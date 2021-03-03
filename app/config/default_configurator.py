a8847df5d11156133c7ce7cf3eda43c204d9b0a4
Steen303 <post+github@alexandersteen.be>
2021-03-03 03:48:47 +0100

    Create default_configurator.py

---
+++ app/config/default_configurator.py
@@ -0,0 +1,35 @@
+from configparser import ConfigParser
+
+config = ConfigParser(allow_no_value=True)
+config.optionxform = str
+
+
+def restore():
+    set_default_config()
+    write_file()
+
+
+def write_file():
+    config.write(open('conf/settings.cfg', 'w'))
+
+
+def set_default_config():
+    config['DEFAULT'] = {'readOnlySection': 'false', }
+    config['discogs'] = {}
+    config['discogs']['token'] = 'LpYGwMzazdfdxAHCTRZhStSPMeyEVGNMYynuACzD'
+
+    config['database'] = {}
+    database = config['database']
+    database['useLocalDB'] = 'yes'
+    database['dbFileLocation'] = '../resources/music.db'
+    database['url'] = ''
+    database['username'] = ''
+    database['password'] = ''
+    database['readOnlySection'] = 'true'
+
+    config['music-library'] = {}
+    music_lib = config['music-library']
+    music_lib['run_filescan'] = 'true'
+    music_lib['library-root'] = 'D:\Music\GOA\Old School'
+    music_lib['artist-albums-dir'] = '%(library-root)s\Artist Releases'
+    music_lib['artist-albums-dir-test'] = '%(library-root)s\Artist Releases\Adrenalin Drum'
